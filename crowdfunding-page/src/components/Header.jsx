0af491db265ca8df46212039e4b510a32c8cf208
Thiagoramos456 <thiagovirtual10@gmail.com>
2021-10-16 18:40:00 -0300

    Criando header

---
+++ crowdfunding-page/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+export class Header extends Component {
+    render() {
+        return (
+            <header>
+                <section className="header-title">
+                    <h1>crowdfund</h1>
+                </section>
+                <ul className="header-menu">
+                    <li className="menu-item"></li>
+                    <li className="menu-item"></li>
+                    <li className="menu-item"></li>
+                </ul>
+            </header>
+        )
+    }
+}
+
+export default Header;
